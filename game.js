4dc78d4350b2403dd84bca00fcb54a6a9ca3bcc0
Michelle Van <mjtvan@gmail.com>
2019-10-22 17:51:01 +0100

    collect coins

--- game.js
+++ game.js
@@ -22,16 +22,15 @@ let cursors;
 let mysprite;
 let grounds;
 let groundBlock1;
+let coin1;
+let coins;
 
 
 function preload() {
-    // game.load.onLoadStart.add(loadStart, this);
-    // game.load.onFileComplete.add(fileComplete, this);
-    // game.load.onLoadComplete.add(loadComplete, this);
 
     this.load.spritesheet('character', 'assets/sprite_run.png', {
             frameWidth: 31, 
-            frameHeight: 25
+            frameHeight: 25,
     });
     this.load.image('treebackground', 'assets/treebackground.png');
     this.load.spritesheet('coin', 'assets/coin.png', {
@@ -47,10 +46,14 @@ function preload() {
     this.load.image('bigblock', 'assets/big_ground.png');
     this.load.image('redbig', 'assets/red_bigsquare.png');
     this.load.image('redsmall', 'assets/red_smallsquare.png');
+    this.load.spritesheet('turnip', 'assets/turnip.png', {
+            frameWidth: 16.4,
+            frameHeight: 26,
+    });
 }
 
 function create() {
-    // game.physics.startSystem(Phaser.Physics.ARCADE);
+
     let bg = this.add.image(0, 0, 'treebackground');
     bg.setOrigin(0, 0);
     bg.setScale(5.5);
@@ -71,41 +74,13 @@ function create() {
     static.create(212, 180, 'grey_block').setScale(3);
     static.create(313, 625, 'ground_block').setScale(2.8);
     static.create(450, 578, 'bigblock').setScale(2.8);
-    static.create(650, 420, 'redbig').setScale(2.3);
-    static.create(614, 411, 'redsmall').setScale(2.3);
-    static.create(686, 411, 'redsmall').setScale(2.3);
+    static.create(700, 480, 'redbig').setScale(2.3);
+    static.create(664, 471, 'redsmall').setScale(2.3);
+    static.create(736, 471, 'redsmall').setScale(2.3);
 
     let tree1 = this.add.image(108, 490, 'tree');
     tree1.setOrigin(0, 0);
     tree1.setScale(3.5);
-
-    // let greyBlock1 = this.add.image(120, 180, 'grey_block');
-    // greyBlock1.setScale(3);
-
-    // let greyBlock2 = this.add.image(166, 180, 'grey_block');
-    // greyBlock2.setScale(3);
-
-    // let greyBlock3 = this.add.image(212, 180, 'grey_block');
-    // greyBlock3.setScale(3);
-
-    // groundBlock1 = this.add.image(313, 625, 'ground_block');
-    // groundBlock1.setScale(2.8);
-    // groundBlock1.physics.add.staticBody();
-    // // groundBlock1.body.immovable = true;
-    // this.physics.add.collider(mysprite, groundBlock1); 
-
-
-    // let bigBlock1 = this.add.image(450, 578, 'bigblock');
-    // bigBlock1.setScale(2.8);
-
-    // let redBigSquare1 = this.add.image(650, 420, 'redbig');
-    // redBigSquare1.setScale(2.3);
-
-    // let redSmallSquare1 = this.add.image(614, 411, 'redsmall');
-    // redSmallSquare1.setScale(2.3);
-
-    // let redSmallSquare2 = this.add.image(686, 411, 'redsmall');
-    // redSmallSquare2.setScale(2.3);
 
     mysprite = this.physics.add.sprite(50, 380, 'character');
     mysprite.setScale(2.4);
@@ -132,27 +107,16 @@ function create() {
         frameRate: 50
     })
 
-    mysprite.body.setGravityY(50);
+    mysprite.body.setGravityY(80);
+    // mysprite.body.velocity.x = 100;
 
     this.physics.add.collider(mysprite, static); 
 
     cursors = this.input.keyboard.createCursorKeys();
 
     /////NEED TO REFACTOR////////
-    // let coins = this.add.group();
-    // coins.create(260, 358, 'coin').setScale(1.2);
-    // coins.create(350, 290, 'coin').setScale(1.2);
-    // coins.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3], 6, true);
-    // coins.callAll('animations.play', 'animations', 'spin');
-    // this.anims.create({
-    //     key: 'spin',
-    //     repeat: -1,
-    //     frameRate: 6,
-    //     frames: this.anims.generateFrameNames('coin', {start: 0, end: 3})
-    // });
-    // coins.play('spin');
-    
-    let coin1 = this.add.sprite(170, 470, 'coin');
+
+    coin1 = this.physics.add.sprite(170, 470, 'coin');
     coin1.setScale(1.8);
     this.anims.create({
         key: 'spin',
@@ -161,8 +125,15 @@ function create() {
         frames: this.anims.generateFrameNames('coin', {start: 0, end: 3})
     });
     coin1.play('spin');
+    coin1.body.allowGravity = false;
+    this.physics.add.overlap(mysprite, coin1, collectCoin, null, this);
 
-    let coin2 = this.add.sprite(314, 585, 'coin');
+    function collectCoin(mysprite, coin1){
+        coin1.disableBody(true, true);
+    }
+    
+
+    let coin2 = this.physics.add.sprite(314, 585, 'coin');
     coin2.setScale(1.8);
     this.anims.create({
         key: 'spin',
@@ -171,8 +142,14 @@ function create() {
         frames: this.anims.generateFrameNames('coin', { start: 0, end: 3 })
     });
     coin2.play('spin');
+    coin2.body.allowGravity = false;
+    this.physics.add.overlap(mysprite, coin2, collectCoin, null, this);
 
-    let coin3 = this.add.sprite(406, 493, 'coin');
+    function collectCoin(mysprite, coin2) {
+        coin2.disableBody(true, true);
+    }
+
+    let coin3 = this.physics.add.sprite(406, 493, 'coin');
     coin3.setScale(1.8);
     this.anims.create({
         key: 'spin',
@@ -181,8 +158,14 @@ function create() {
         frames: this.anims.generateFrameNames('coin', { start: 0, end: 3 })
     });
     coin3.play('spin');
+    coin3.body.allowGravity = false;
+    this.physics.add.overlap(mysprite, coin3, collectCoin, null, this);
 
-    let coin4 = this.add.sprite(450, 493, 'coin');
+    function collectCoin(mysprite, coin3) {
+        coin3.disableBody(true, true);
+    }
+
+    let coin4 = this.physics.add.sprite(450, 493, 'coin');
     coin4.setScale(1.8);
     this.anims.create({
         key: 'spin',
@@ -191,8 +174,14 @@ function create() {
         frames: this.anims.generateFrameNames('coin', { start: 0, end: 3 })
     });
     coin4.play('spin');
+    coin4.body.allowGravity = false;
+    this.physics.add.overlap(mysprite, coin4, collectCoin, null, this);
 
-    let coin5 = this.add.sprite(493, 493, 'coin');
+    function collectCoin(mysprite, coin4) {
+        coin4.disableBody(true, true);
+    }
+
+    let coin5 = this.physics.add.sprite(493, 493, 'coin');
     coin5.setScale(1.8);
     this.anims.create({
         key: 'spin',
@@ -201,9 +190,15 @@ function create() {
         frames: this.anims.generateFrameNames('coin', { start: 0, end: 3 })
     });
     coin5.play('spin');
+    coin5.body.allowGravity = false;
+    this.physics.add.overlap(mysprite, coin5, collectCoin, null, this);
+
+    function collectCoin(mysprite, coin5) {
+        coin5.disableBody(true, true);
+    }
   
 
-    let coin6 = this.add.sprite(676, 385, 'coin');
+    let coin6 = this.physics.add.sprite(680, 445, 'coin');
     coin6.setScale(1.8);
     this.anims.create({
         key: 'spin',
@@ -212,9 +207,15 @@ function create() {
         frames: this.anims.generateFrameNames('coin', { start: 0, end: 3 })
     });
     coin6.play('spin');
+    coin6.body.allowGravity = false;
+    this.physics.add.overlap(mysprite, coin6, collectCoin, null, this);
+
+    function collectCoin(mysprite, coin6) {
+        coin6.disableBody(true, true);
+    }
 
 
-    let coin7 = this.add.sprite(624, 385, 'coin');
+    let coin7 = this.physics.add.sprite(720, 445, 'coin');
     coin7.setScale(1.8);
     this.anims.create({
         key: 'spin',
@@ -223,18 +224,21 @@ function create() {
         frames: this.anims.generateFrameNames('coin', { start: 0, end: 3 })
     });
     coin7.play('spin');
+    coin7.body.allowGravity = false;
+    this.physics.add.overlap(mysprite, coin7, collectCoin, null, this);
 
-    // this.physics.add.overlap(mysprite, coin1, coinTouch, null, this);
-    // function coinTouch (mysprite, coin1){
-    //     coin1.disableBody(true, true);
-    // }
+    function collectCoin(mysprite, coin7) {
+        coin7.disableBody(true, true);
+    }
 
-    this.physics.add.collider(mysprite, coin1);
-    // this.physics.add.collider(mysprite, groundBlock1);
-    // // groundBlock1.setImmovable(true);
-    // this.physics.add.collider(mysprite, groundBlock1);
-    // this.physics.world.collide(mysprite, groundBlock1);
+    let turnip1 = this.physics.add.sprite(540, 360, 'turnip').setScale(2.3); 
+    turnip1.body.allowGravity = false;  
 
+    let turnip2 = this.physics.add.sprite(430, 290, 'turnip').setScale(2.3);
+    turnip2.body.allowGravity = false;  
+    
+    let turnip3 = this.physics.add.sprite(320, 220, 'turnip').setScale(2.3);
+    turnip3.body.allowGravity = false;  
 }
 
 function update() {
@@ -244,7 +248,7 @@ function update() {
         mysprite.setVelocityX(-160);
         mysprite.anims.play('left', true);
     } else if (cursors.right.isDown){
-        mysprite.setVelocityX(160);
+        mysprite.setVelocityX(300);
         mysprite.anims.play('right', true);
     } else {
         mysprite.setVelocityX(0);
@@ -255,10 +259,7 @@ function update() {
         mysprite.setVelocityY(-200);  
         }
 
-    // this.physics.arcade.enable(mysprite);
-    // mysprite.enableBody = true; 
-    // mysprite.body.immovable = true;
-    // this.game.physics.arcade.collide(mysprite, grounds)
+
 };
 
 
